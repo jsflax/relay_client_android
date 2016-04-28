e08cbd048a72b7387141f7a3219239ad9ecd53ee
Jason Flax <jsflax@gmail.com>
2016-04-28 01:00:18 -0400

    updated reame

--- service/src/main/java/com/relay/service/Config.kt
+++ service/src/main/java/com/relay/service/Config.kt
@@ -1,5 +1,8 @@
 package com.relay.service
 
+import com.relay.service.base.Dispatch
+import com.relay.service.base.GET
+
 class Config(val host: String) {
 }
 
@@ -22,6 +25,11 @@ object RelayService {
 
     fun init(host: String) {
         config = Config(host)
+
+        Thread {
+            // ping host
+            Dispatch.sendRequest<Any>("", GET()) {}
+        }
     }
 
     val channels = Channels
